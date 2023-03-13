0810b1322f31f1941338e3ea2ae4e1a30777fb47
WessonX <wessonx@163.com>
2023-03-13 23:06:26 +0800

    fix

--- peripheral/characteristic.js
+++ peripheral/characteristic.js
@@ -4,8 +4,6 @@ var bleno = require('bleno')
 
 var BlenoCharacteristic = bleno.Characteristic
 
-// 自定义的NetCharacteristic继承自bleno的BlenoCharacteristic
-util.inherits(NetCharacteristic,BlenoCharacteristic)
 
 // 自定义属性的构造函数
 var NetCharacteristic = function() {
@@ -15,14 +13,17 @@ var NetCharacteristic = function() {
         descriptor:[ new bleno.Descriptor({
             uuid: '2901',
             value: 'set wifi ssid and pwd.'
-          })]
+        })]
     });
     // 网络的ssid
     this.SSID = Buffer.alloc(0)
-
+    
     // 网络密码
     // this.PassWord = Buffer.alloc(0)
 };
+
+// 自定义的NetCharacteristic继承自bleno的BlenoCharacteristic
+util.inherits(NetCharacteristic,BlenoCharacteristic);
 
 // NetCharacteristic.prototype.onReadRequest = function(offset,callback) {
 //     console.log("NetCharacteristic read Req: ssid = " + this.SSID + " pwd = " + this.PassWord )
