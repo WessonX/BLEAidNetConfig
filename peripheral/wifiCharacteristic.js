e5752bc936db24d7052a97baaeaab57dd1752cba
WessonX <wessonx@163.com>
2023-03-16 14:00:43 +0800

    add:添加联网功能

--- peripheral/wifiCharacteristic.js
+++ peripheral/wifiCharacteristic.js
@@ -4,6 +4,11 @@ var bleno = require('bleno')
 
 var BlenoCharacteristic = bleno.Characteristic
 
+const wifi = require('node-wifi')
+
+wifi.init({
+    iface:null
+});
 
 // 自定义属性的构造函数
 var wifiCharacteristic = function() {
@@ -29,6 +34,11 @@ wifiCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResp
     var wifiData_obj = JSON.parse(wifiData_str)
     console.log("onWriteRequest: ssid = " + wifiData_obj.ssid + " pwd:" + wifiData_obj.pwd)
     callback(this.RESULT_SUCCESS)
+
+    // Connect to a network
+    wifi.connect({ ssid: wifiData_obj.ssid, password:wifiData_obj.pwd }, () => {
+        console.log('Connected'); 
+    });
 }
 
 module.exports = wifiCharacteristic
